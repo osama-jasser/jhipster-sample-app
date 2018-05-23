ced8b0b6d92886f02cbb92fa4b7300361d8fafae
Liam Newman <bitwiseman@gmail.com>
2018-05-23 09:47:51 -0700

    Final tweaks

--- jenkins/zero-to-cd/deploy-multi-stage.groovy
+++ jenkins/zero-to-cd/deploy-multi-stage.groovy
@@ -1,11 +1,14 @@
 stage('Deploy to Staging') {
+  agent any
   steps {
+
     sh './deploy.sh staging'
     sh 'echo Notifying the Team!'
   }
 }
 
 stage('Deploy to Production') {
+  agent any
   input {
     message 'Deploy to production?'
     ok 'Fire away!'
